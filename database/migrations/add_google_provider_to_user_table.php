971deb3e12c64dd1fb065c4455aea934df390d04
Kristof Serré <kristof@statik.be>
2018-12-10 13:35:13 +0100

    Rework of the migrations autoload in ServiceProvider

--- database/migrations/add_google_provider_to_user_table.php
+++ database/migrations/add_google_provider_to_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddGoogleProviderToUserTable extends Migration
+class addgoogleprovidertousertable extends Migration
 {
     /**
      * Run the migrations.
--- src/GoogleAuthenticateServiceProvider.php
+++ src/GoogleAuthenticateServiceProvider.php
@@ -15,7 +15,10 @@ class GoogleAuthenticateServiceProvider extends ServiceProvider
     {
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'statikbe');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'Statikbe');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->publishes([
+            __DIR__.'/../database/migrations/add_google_provider_to_user_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_add_google_provider_to_user_table.php'),
+        ], 'migrations');
+
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
